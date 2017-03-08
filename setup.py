9fd55efe57077dbb236855655f041af392d728a7
kurozumi <kurozumi@internetmarketing.co.jp>
2017-03-08 12:57:16 +0900

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
-setup(
-    name="mws",
-    version="1.0",
-    description="wrapper for MWS",
-    packages=["mws"],
-    install_requires=[
-        "cchardet",
-        "PySocks"
-    ]
-)
+if __name__ == "__main__":
+    setup(
+        name="mws",
+        version="1.0",
+        description="wrapper for MWS",
+        packages=["mws"],
+        install_requires=[
+            "cchardet",
+            "PySocks"
+        ]
+    )
